2535d021a1b3326ce98b54dd773d166458b16455
pbenderson <souza98120@gmail.com>
2021-11-23 21:11:58 -0300

    Initial commit

---
+++ aula06/script.js
@@ -0,0 +1,12 @@
+
+/*
+var nome = window.prompt('Qual é seu nome')
+window.prompt('É um grande prazer te conhecer ' + nome + '!')
+*/
+
+var n1 = Number.parseInt(window.prompt('Infome 1 numero:'))
+var n2 = Number.parseInt(window.prompt('Informe outro numero:'))
+
+var result = n1 + n2
+
+var n2 = window.alert('Resultado da soma foi ' + result)
