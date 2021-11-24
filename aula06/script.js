f2eb50624cc9d8932ecc8a91e12bdc8dc5718925
pbenderson <souza98120@gmail.com>
2021-11-24 14:36:04 -0300

    .

--- aula06/script.js
+++ aula06/script.js
@@ -2,11 +2,23 @@
 /*
 var nome = window.prompt('Qual é seu nome')
 window.prompt('É um grande prazer te conhecer ' + nome + '!')
+
+//Conversão de Number para String
+
+    String(n)
+    n.toString()
+
 */
 
-var n1 = Number.parseInt(window.prompt('Infome 1 numero:'))
-var n2 = Number.parseInt(window.prompt('Informe outro numero:'))
+/*
+s.length quantas caracteristicas tem a string
+s.toUpperCase() todas as as letras minusculas
+s.toLowerCase() todas as letras minusculas
+
+*/
+var n1 = Number(window.prompt('Infome 1 numero:'))//paserInt converte para number
+var n2 = Number(window.prompt('Informe outro numero:'))//paserInt converte para number
 
 var result = n1 + n2
 
-var n2 = window.alert('Resultado da soma foi ' + result)
+var n2 = window.alert(`A soma entre ${n1} e ${n2} é igual a ${result}`)
---
+++ aula06/script06.js
@@ -0,0 +1,5 @@
+
+var nome = window.prompt('qual é o seu nome?')
+document.write(`Olá, ${nome} seu nome tem ${nome.length} letras<br>`)
+document.write(`Seu nome em letras maisculas  ${nome.toUpperCase()}<br>`)
+document.write(`Seu nome em letras minusculas  ${nome.toLocaleLowerCase()}<br>`)
